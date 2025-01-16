b1ff6aa2e6aa76b7e97b552eb70eb5d6756427d7
tarabanamx <luis@tarabana.mx>
2025-01-15 20:23:49 -0600

    Primer commit

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/",
   plugins: [react()],
-  base: "./", // Asegura que las rutas sean relativas
-  build: {
-    outDir: "dist", // Directorio de salida para el build
-    assetsDir: "assets", // Directorio para recursos estáticos
-  },
-  assetsInclude: ["**/*.jpg", "**/*.png", "**/*.gif", "**/*.svg", "**/*.webp"], // Incluye formatos adicionales
 });

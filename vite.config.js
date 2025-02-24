48230a78b76e6bcb3fef12c79008f155859704bb
tarabanamx <luis@tarabana.mx>
2025-02-23 21:50:33 -0600

    Actualización del proyecto para despliegue en Vercel

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  root: "frontend", // Define la raíz del proyecto
+  plugins: [react()],
   build: {
-    outDir: "dist", // Output en frontend/dist
+    outDir: "dist",
   },
   server: {
     fs: {
-      allow: [".."], // Permite acceder a archivos fuera de frontend/
+      allow: ["."],
     },
   },
 });

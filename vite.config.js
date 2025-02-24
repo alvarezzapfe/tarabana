8670327547ecf163ab40981d33daaf337fc215eb
tarabanamx <luis@tarabana.mx>
2025-02-24 09:20:27 -0600

    Fix preview script and deploy setup

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,9 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./",
+  base: "./", // Importante para que los archivos se sirvan correctamente en Vercel
   build: {
-    outDir: "dist",
-  },
-  server: {
-    fs: {
-      allow: ["."], // Permite acceso a archivos en la raíz
-    },
+    outDir: "dist", // La carpeta donde se guarda la compilación
+    assetsDir: "assets", // Para evitar conflictos con los assets
   },
 });

a232d776ae194a108d64fad64741a168397f31fa
Luis Armando Alvarez Zapfe <lalvarezzapfe@MacBook-Air-de-Luis.local>
2025-12-18 10:46:18 -0600

    login

--- src/login.jsx
+++ src/login.jsx
@@ -1,7 +1,7 @@
 // src/login.jsx
 import React, { useState } from "react";
 import Navbar from "./components/Navbar";
-import Footer from "./components/footer";
+import Footer from "./components/Footer";
 import "./assets/css/login.css";
 
 const Login = () => {
